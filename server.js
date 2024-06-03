7e9b32d6eea57b893703f70a5148435950b3088a
Sam Gomez <softwaresamdev@gmail.com>
2024-06-02 19:01:44 -0600

    added middleware

--- server.js
+++ server.js
@@ -17,6 +17,14 @@ MongoClient.connect(dbconnectionString)
         collection = db.collection('movies')
     })
 
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+app.use(cors())
+
+
+
     app.listen(process.env.PORT || PORT, () => {
         console.log(`Server is running on port ${process.env.PORT}`)
     })
